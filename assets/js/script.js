d7a7f6de2bd07ec06d9509fa16367e288b113001
Emily Hill <emhillmed2021@gmail.com>
2022-09-12 13:26:38 -0500

    create outline in js, set time variables, and created hour array

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,32 @@
 
     var currentDate =  moment().format("MMMM Do YYYY, h:mm:ss a");
     document.getElementById("currentDay").textContent = currentDate 
+
+    //set time in schedule using moment.js
+    var time9 = moment().time(9)
+    var time10 = moment().time(10)
+    var time11 = moment().time(11)
+    var time12 = moment().time(12)
+    var time1 = moment().time(1)
+    var time2 = moment().time(2)
+    var time3 = moment().time(3)
+    var time4 = moment().time(4)
+    var time5 = moment().time(5)
+
+
+    //array hour blocks 
+
+    var hour = [time9, time10, time11, time12, time1, time2, time3, time4, time5,]
+
+    //get local storage 
+
+    
+
+    //generate all timeblocks and insert saved local storage 
+
+    //audit each time to display past, present, future 
+
+    //delete event function for each timeblock 
+
+    //save event button for each timeblock 
+
