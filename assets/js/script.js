f03b599ab63af7ab07abbe40ba2326a2f7471a65
Emily Hill <emhillmed2021@gmail.com>
2022-09-13 13:52:21 -0500

    trying to fix local storage and broken moment().

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,20 +3,20 @@ var currentDate = moment().format("MMMM Do YYYY, h:mm:ss a");
 document.getElementById("currentDay").textContent = currentDate
 
 //set time in schedule using moment.js
-var time9 = document.getElementById("#hour-9"); 
-var time10 = document.getElementById("#10");
-var time11 = document.getElementById("#11"); 
-var time12 = document.getElementById("#12"); 
-var time1 = document.getElementById("#1"); 
-var time2 = document.getElementById("#2"); 
-var time3 = document.getElementById("#3"); 
-var time4 = document.getElementById("#4"); 
-var time5 = document.getElementById("#5"); 
+var time9 = moment().hour(9); 
+var time10 = moment().hour(10); 
+var time11 = moment().hour(11);
+var time12 = moment().hour(12); 
+var time1 = moment().hour(1); 
+var time2 = moment().hour(2);  
+var time3 = moment().hour(3); 
+var time4 = moment().hour(4); 
+var time5 = moment().hour(5); 
 
 
 //array hour blocks 
 
-var hourBlock = [time9, time10, time11, time12, time1, time2, time3, time4, time5,]
+//var hourBlock = [time9, time10, time11, time12, time1, time2, time3, time4, time5,]
 
 // var currentHour= JSON.parse(hourBlock); 
 //set local storage 
@@ -25,15 +25,12 @@ var hourBlock = [time9, time10, time11, time12, time1, time2, time3, time4, time
 
 $("#button-save").on("click",function() {
     console.log("clicked")
-    $("textarea[type='textbox']").each(function() {
-        var id = $(this).attr("hour-9");
-        var value = $(this).val(); 
-        localStorage.setItem(id, value); 
-
-    }
-    );
-
-}); 
+    var hour = document.getElementById("daily-form").value;
+    var hourAll = JSON.parse(localStorage.getItem(hourBlock.length)) || []; 
+    hourAll.push(hour);
+    localStorage.setItem("hourAll", JSON.stringify(hourAll.length)); 
+}
+); 
 
 
 
@@ -47,27 +44,8 @@ $("#button-save").on("click",function() {
 // $("time10").html(localStorage.setItem);
 
 
-// localStorage.setItem("time11", JSON.stringify(time11));
-// $("time11").html(localStorage.setItem);
 
 
-// localStorage.setItem("time12", JSON.stringify(time12));
-// $("time12").html(localStorage.setItem);
-
-// localStorage.setItem("time1", JSON.stringify(time1));
-// $("time1").html(localStorage.setItem);
-
-// localStorage.setItem("time2", JSON.stringify(time2));
-// $("time2").html(localStorage.setItem);
-
-// localStorage.setItem("time3", JSON.stringify(time3));
-// $("time3").html(localStorage.setItem);
-
-// localStorage.setItem("time4", JSON.stringify(time4));
-// $("time4").html(localStorage.setItem);
-
-// localStorage.setItem("time5", JSON.stringify(time5));
-// $("time5").html(localStorage.setItem); 
 
 
 
@@ -150,7 +128,7 @@ function timeIndicator() {
     }
     else {
         $("#hour-9").addClass("future"); 
-    }
+    };
 
     if (moment().isBetween(time10, time11)) {
         $("#hour-10").addClass("present");
@@ -160,7 +138,7 @@ function timeIndicator() {
     }
     else {
         $("#hour-10").addClass("future"); 
-    }
+    };
 
     if (moment().isBetween(time11, time12)) {
         $("#hour-11").addClass("present");
@@ -170,7 +148,7 @@ function timeIndicator() {
     }
     else {
         $("#hour-11").addClass("future"); 
-    }
+    };
 
     if (moment().isBetween(time12, time1)) {
         $("#hour-12").addClass("present");
@@ -180,7 +158,7 @@ function timeIndicator() {
     }
     else {
         $("#hour-12").addClass("future"); 
-    }
+    };
 
     if (moment().isBetween(time1, time2)) {
         $("#hour-1").addClass("present");
@@ -190,7 +168,7 @@ function timeIndicator() {
     }
     else {
         $("#hour-1").addClass("future"); 
-    }
+    };
 
     if (moment().isBetween(time2, time3)) {
         $("#hour-2").addClass("present");
@@ -200,7 +178,7 @@ function timeIndicator() {
     }
     else {
         $("#hour-2").addClass("future"); 
-    }
+    };
 
     if (moment().isBetween(time3, time4)) {
         $("#hour-3").addClass("present");
@@ -210,7 +188,7 @@ function timeIndicator() {
     }
     else {
         $("#hour-3").addClass("future"); 
-    }
+    };
 
     if (moment().isBetween(time4, time5)) {
         $("#hour-4").addClass("present");
@@ -220,7 +198,7 @@ function timeIndicator() {
     }
     else {
         $("#hour-4").addClass("future"); 
-    }
+    };
 
     if (moment().isSame(time5)) {
         $("#hour-5").addClass("present");
@@ -230,7 +208,7 @@ function timeIndicator() {
     }
     else {
         $("#hour-5").addClass("future"); 
-    }
-}
+    };
+};
 
 timeIndicator(); 
